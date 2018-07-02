ade83d712ceabfcd56ab969db56c1eab26da8e12
Charles Iliya Krempeaux <iamreiver@gmail.com>
2018-07-02 02:16:28 -0700

    errors

---
+++ errors.go
@@ -0,0 +1,11 @@
+package utf8s
+
+import (
+	"errors"
+)
+
+var (
+	errInternalError = errors.New("Internal Error")
+	errInvalidUTF8   = internalInvalidUTF8Complainer{}
+	errNilReader     = internalNilReaderComplainer{}
+)
